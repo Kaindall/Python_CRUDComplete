6cee86dbba37f107695d1216bf2974aaa1c0e24d
Kaindall <gustavozanete2@gmail.com>
2023-06-16 16:58:41 -0300

    feature: some new lines to adjust update_row method

--- test.py
+++ test.py
@@ -1,14 +1,37 @@
-cols = ("Cname", "Csurname", "Expenses", "Gender", "Nationality")
+cols = ("idClient", "Cname", "Csurname", "Expenses", "Gender", "Nationality")
 
-data = (("Jacki","Enders",4482,"Female","Panamanian"),
-("Allegra","Ucchino",9814,"Female","Eskimo"),
-("Barbabas","Collingham",5933,"Male","Panamanian"))
+data = ((2, "Jacki","Enders",4482,"Female","Panamanian"),
+(3, "Allegra","Ucchino",9814,"Female","Eskimo"),
+(4, "Barbabas","Collingham",5933,"Male","Panamanian"))
 
-       
-for row in data:
-       for e, element in enumerate(row):
-              if type(element) == str:
-                     print (f'{cols[e]} = "{element}"')
-              else:
-                     print (f'{cols[e]} = {element}')
+table = "client"       
+
+
+def quickfunc (cols, data):
+       for row in data:
+              line = ""
+              
+              for e, element in enumerate(row):
+                     if type(element) == str:
+                            if len(line) == 0:
+                                   line = line + (f'{cols[e]} = "{element}"')
+                            else:
+                                   line = line + (f', {cols[e]} = "{element}"')
+                     else:
+                            if len(line) == 0:
+                                   line = line + (f'{cols[e]} = {element}')
+                            else:
+                                   line = line + (f', {cols[e]} = {element}')
+              return line
+
+def quickfunc2 (cols, data):
+       print(f'''
+              INSERT INTO {table}({cols})\n
+              VALUES {data}
+              ON DUPLICATE KEY UPDATE
+              
+              ''')
+              
+              
+quickfunc2 (cols, data)
        
